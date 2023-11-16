55b93ecd41a5d586ce0dc4b457469d04a8b809ed
Léo Victor Cruz Vasconcelos <leotpw9@gmail.com>
2023-11-15 22:28:00 -0300

    Corrigir simbolos

--- Server/Baralho.py
+++ Server/Baralho.py
@@ -22,7 +22,7 @@ def getBaralho():
         13:'K'
     }
 
-    for naipe in ('Ou', 'Es', 'Co', 'Pa'):
+    for naipe in (chr(9830), chr(9824), chr(9829), chr(9827)):
         for valor in range(1,14):
             baralho.append(f'{valor}{naipe}') if valor > 1 and valor < 11 else baralho.append(f'{hops[valor]}{naipe}')
     
